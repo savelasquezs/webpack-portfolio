81b186b844d348a619b17fff88032737116c178a
Santiago Velásquez <santyvano@outlook.com>
2023-01-31 06:59:22 -0500

    ci v6

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,6 @@
-const fs = require('fs');
-fs.writeFile('./.env', `API=${process.env.API}\n`);
+// **create-env.js**
+
+const fs = require('fs'); // fs = file system
+
+// fs.writeFileSync("path", `argumento a crear`);
+fs.writeFileSync('./.env', `API=${process.env.API}\n`);
